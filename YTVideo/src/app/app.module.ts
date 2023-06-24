183732bfad7a13fe9410ed9d43dd04f03086c621
Gazi Monir-Uz-Zaman Apu <ciphercall@gmail.com>
2023-06-24 11:53:24 +0600

    Video listing page
    Added Video thumb, video listing with names and descriptions to the page.

--- YTVideo/src/app/app.module.ts
+++ YTVideo/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VideoHomeComponent } from './video-home/video-home.component';
+import { MatCardModule } from '@angular/material/card';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { VideoHomeComponent } from './video-home/video-home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- YTVideo/src/app/video-home/video-home.component.ts
+++ YTVideo/src/app/video-home/video-home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-video-home',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./video-home.component.css']
 })
 export class VideoHomeComponent {
+  videos!: any[];
 
+  constructor(private http: HttpClient) {
+    this.http.get<any>('/assets/videos.json').subscribe(data => {
+      this.videos = data.results;
+    });
+  }
 }
