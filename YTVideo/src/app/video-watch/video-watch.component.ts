a31709c497c3d6cbfe27038b93dbf80ecbf2b939
Gazi Monir-Uz-Zaman Apu <ciphercall@gmail.com>
2023-06-24 17:22:16 +0600

    video watch page
    Primary backend config done

--- YTVideo/src/app/video-watch/video-watch.component.ts
+++ YTVideo/src/app/video-watch/video-watch.component.ts
@@ -17,6 +17,7 @@ interface Video {
 export class VideoWatchComponent implements OnInit {
   videoId!: string;
   video!: any;
+  suggestedVideos!: any[];
 
   constructor(private route: ActivatedRoute, private http: HttpClient) {}
 
@@ -24,13 +25,21 @@ export class VideoWatchComponent implements OnInit {
     this.route.paramMap.subscribe(params => {
       this.videoId = params.get('id')!;
       this.loadVideo(this.videoId);
+      this.loadSuggestedVideos(this.videoId);
     });
   }
 
   loadVideo(videoId: string) {
     // Load the video data here and store it in the `video` property
     this.http.get<any>('/assets/videos.json').subscribe(data => {
-      this.video = data.results.find((video:Video) => video.id === videoId);
+      this.video = data.results.find((video: any) => video.id === videoId);
+    });
+  }
+
+  loadSuggestedVideos(videoId: string) {
+    // Load the suggested videos here and store them in the `suggestedVideos` property
+    this.http.get<any>('/assets/videos.json').subscribe(data => {
+      this.suggestedVideos = data.results.filter((video: any) => video.id !== videoId);
     });
   }
 }
